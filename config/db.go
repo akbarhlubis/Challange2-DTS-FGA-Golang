508cf5817c08f03585b5bfe24cf376100ae9a6c4
akbarhlubis <akbar_hamonangan_lubis@teknokrat.ac.id>
2024-03-09 11:25:43 +0700

    Add Person model and controller

--- config/db.go
+++ config/db.go
@@ -1,1 +1,16 @@
 package config
+
+import (
+	"challange1_9Mar/model"
+
+	"github.com/jinzhu/gorm"
+)
+
+func DBInit() *gorm.DB {
+	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/godb?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(model.Person{})
+	return db
+}
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"challange1_9Mar/config"
+	"challange1_9Mar/controller"
 
-func main(){
-	fmt.Println("Tes")
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db := config.DBInit()
+	inDB := &controller.InDB{DB: db}
+
+	router := gin.Default()
+
+	router.GET("person/:id", inDB.GetPerson)
+	router.GET("persons", inDB.GetPersons)
+	router.POST("person", inDB.CreatePerson)
+	router.PUT("person", inDB.UpdatePerson)
+	router.DELETE("person/:id", inDB.DeletePerson)
+	router.Run(":8080")
 }
